ccc561c95a3bc4f638e8f974cfa2b7854a789e56
daviidxu <david_xu_@hotmail.fr>
2024-07-06 21:26:12 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return (
+    <main className="">
+      <div className="h-[100vh]"></div>
+    </main>
+  );
+}
